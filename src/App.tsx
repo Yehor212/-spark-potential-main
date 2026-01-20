0029b76dcfe997fa529048dad48d4efec4765bb4
Yehor Shamraiev <egorsamraev@gmail.com>
2026-01-20 00:27:28 -0500

    Add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename={import.meta.env.BASE_URL || '/'}>
             <AppRoutes />
           </BrowserRouter>
         </TooltipProvider>
